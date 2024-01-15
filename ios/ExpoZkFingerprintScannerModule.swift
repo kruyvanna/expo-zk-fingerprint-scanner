a3329881e579aa07fa48bfe64e93857c4a458fcd
Kruy Vanna <kruyvanna@gmail.com>
2024-01-15 13:47:15 +0700

    add code for ios

--- ios/ExpoZkFingerprintScannerModule.swift
+++ ios/ExpoZkFingerprintScannerModule.swift
@@ -9,28 +9,26 @@ public class ExpoZkFingerprintScannerModule: Module {
     // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
     // The module will be accessible from `requireNativeModule('ExpoZkFingerprintScanner')` in JavaScript.
     Name("ExpoZkFingerprintScanner")
+      
+    Events("onDeviceConnected", "onDeviceDisconnected", "onGotImage")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants([
-      "PI": Double.pi
-    ])
+      
+    AsyncFunction("connectDevice") {
+            
+    }
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
+    AsyncFunction("disconnectDevice") {
+        
     }
 
     // Defines a JavaScript function that always returns a Promise and whose native code
     // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { (value: String) in
-      // Send an event to JavaScript.
-      self.sendEvent("onChange", [
-        "value": value
-      ])
-    }
+//    AsyncFunction("setValueAsync") { (value: String) in
+//      // Send an event to JavaScript.
+//      self.sendEvent("onChange", [
+//        "value": value
+//      ])
+//    }
 
     // Enables the module to be used as a native view. Definition components that are accepted as part of the
     // view definition: Prop, Events.
